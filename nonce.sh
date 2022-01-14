fa0602c17e4238bbf867cd2ecec4a995a411f567
Seal-Clubber <35927816+Seal-Clubber@users.noreply.github.com>
2022-01-14 20:55:39 +0100

    Update nonce.sh

--- nonce.sh
+++ nonce.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-COIN_CLI=monacoin-cli
+COIN_CLI=raven-cli
 
 BLOCKS=$($COIN_CLI getblockcount)
 
 START_BLOCK=$(expr 1 + $(tail -n1 nonce.csv | awk -F , '{print $1}'))
 
+
 for i in `seq $START_BLOCK $BLOCKS`; do
 	BLOCK_HASH=$($COIN_CLI getblockhash $i)
-	NONCE=$($COIN_CLI getblock $BLOCK_HASH | jq .nonce)
+	if [ $i -lt 1219735 ]
+	then
+		NONCE=$($COIN_CLI getblock $BLOCK_HASH | jq .nonce)
+	else
+		NONCE=$($COIN_CLI getblock $BLOCK_HASH | jq .nonce64)
+	fi
 	echo "$i,$NONCE" | tee -a nonce.csv
 done
-
