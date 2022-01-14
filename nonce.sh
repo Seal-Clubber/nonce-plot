e65f2eb0611844404845641b295a6e6f731d0d0c
Seal-Clubber <35927816+Seal-Clubber@users.noreply.github.com>
2022-01-14 21:15:33 +0100

    Update nonce.sh

--- nonce.sh
+++ nonce.sh
@@ -9,7 +9,7 @@ START_BLOCK=$(expr 1 + $(tail -n1 nonce.csv | awk -F , '{print $1}'))
 
 for i in `seq $START_BLOCK $BLOCKS`; do
 	BLOCK_HASH=$($COIN_CLI getblockhash $i)
-	if [ $i -lt 1219735 ]
+	if [ $i -lt 1219736 ]
 	then
 		NONCE=$($COIN_CLI getblock $BLOCK_HASH | jq .nonce)
 	else
